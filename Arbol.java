398ed9e974b530d0a92f288a22a6e9d60cc75c82
MaferOrdonez1 <90990182+MaferOrdonez1@users.noreply.github.com>
2022-05-14 17:21:45 -0600

    Add files via upload

--- Arbol.java
+++ Arbol.java
@@ -4,6 +4,8 @@
  */
 package arbol;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 /**
@@ -20,6 +22,25 @@ public class Arbol {
         Arbol print = new Arbol();
         print.menu();
     }
+    public boolean escompleto(NodoArbol raiz) {
+        if (raiz == null) {
+            return true;
+        }
+        Queue<NodoArbol> queue = new LinkedList<>();
+        // add the root node to queue
+        queue.add(raiz);
+        boolean foundNonComplete = false;
+        while (!queue.isEmpty()) {
+            NodoArbol curr = queue.poll();
+            if (curr.hijoIzquierdo != null) {
+                if (foundNonComplete) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     public void menu(){
         boolean salir = false;
         int opcion;
@@ -46,16 +67,16 @@ public class Arbol {
                case 1:
                    System.out.println("Ingrese elemento para arbol 1:");
                    binaryTree1.addNodo(in.nextInt());
-                   System.out.println("Ingrese elemento para arbol 2:");
-                   binaryTree2.addNodo(in.nextInt());
+                   /*System.out.println("Ingrese elemento para arbol 2:");
+                   binaryTree2.addNodo(in.nextInt());*/
                    
                    break;
                            
                case 2:
                    System.out.println("Ingrese nodo al borrar arbol 1:");
                    binaryTree1.eliminar(in.nextInt());
-                   System.out.println("Ingrese nodo al borrar arbol 2:");
-                   binaryTree2.eliminar(in.nextInt());
+                   /*System.out.println("Ingrese nodo al borrar arbol 2:");
+                   binaryTree2.eliminar(in.nextInt());*/
 
                    break;
                 case 3:
@@ -63,8 +84,8 @@ public class Arbol {
                    System.out.println("Arbol 1");
                    binaryTree1.Preorden(binaryTree1.raiz);
                    System.out.println("\n");
-                   System.out.println("Arbol 2");
-                   binaryTree2.Preorden(binaryTree2.raiz);
+                   /*System.out.println("Arbol 2");
+                   binaryTree2.Preorden(binaryTree2.raiz);*/
                    
                    break;
                 case 4:
@@ -72,8 +93,8 @@ public class Arbol {
                    System.out.println("Arbol 1");
                    binaryTree1.PostOrden(binaryTree1.raiz);
                    System.out.println("\n");
-                   System.out.println("Arbol 2");
-                   binaryTree2.PostOrden(binaryTree2.raiz);
+                   /*System.out.println("Arbol 2");
+                   binaryTree2.PostOrden(binaryTree2.raiz);*/
                    
                    break;
                 case 5:
@@ -81,23 +102,23 @@ public class Arbol {
                    System.out.println("Arbol 1");
                    binaryTree1.Inorden(binaryTree1.raiz);
                    System.out.println("\n");
-                   System.out.println("Arbol 2");
-                   binaryTree2.Inorden(binaryTree2.raiz);
+                   /*System.out.println("Arbol 2");
+                   binaryTree2.Inorden(binaryTree2.raiz);*/
                    
                    break;
                 case 6:
                    System.out.println("Obtener la raíz del arbol 1:"+binaryTree1.ObtenerlaRaíz());
-                   System.out.println("Obtener la raíz del arbol 2:"+binaryTree2.ObtenerlaRaíz());
+                   //System.out.println("Obtener la raíz del arbol 2:"+binaryTree2.ObtenerlaRaíz());
                    
                    break;
                 case 7:
                    System.out.println("La altura del arbol 1 es: " + binaryTree1.Altura(binaryTree1.raiz));
-                   System.out.println("La altura del arbol 2 es: " + binaryTree2.Altura(binaryTree2.raiz));
+                   //System.out.println("La altura del arbol 2 es: " + binaryTree2.Altura(binaryTree2.raiz));
                    
                    break;
                 case 8:
                    System.out.println("Las hojas del arbol 1 son: "+binaryTree1.Contar(binaryTree1.raiz));
-                   System.out.println("Las hojas del arbol 2 son: "+binaryTree2.Contar(binaryTree2.raiz));
+                   //System.out.println("Las hojas del arbol 2 son: "+binaryTree2.Contar(binaryTree2.raiz));
                    
                    break;
                 case 9:
@@ -107,28 +128,30 @@ public class Arbol {
                    }else{
                         System.out.println("Nodo encontrado");      
                    }
-                   if(binaryTree2.Buscar(in.nextInt())== null){
+                   /*if(binaryTree2.Buscar(in.nextInt())== null){
                        System.out.println("Nodo no encontrado");
                    }else{
                         System.out.println("Nodo encontrado");      
-                   }
+                   }*/
                    break;
                 case 10:
-                   System.out.println("Comparar arboles");
+                   /*System.out.println("Comparar arboles");
                    System.out.println(binaryTree1.equals(binaryTree1.raiz, binaryTree2.raiz));
                     if ((binaryTree1.equals(binaryTree1.raiz, binaryTree2.raiz)) == true) {
                         System.out.println("Iguales");
                     }else{
                         System.out.println("Diferentes");
-                    }
+                    }*/
                 case 11:
                     System.out.println("Niveles");
                     System.out.println("Arbol 1");
                     binaryTree1.mostrar_nivel(binaryTree1.raiz);
-                    System.out.println("Arbol 2");
+                    /*System.out.println("Arbol 2");
                     binaryTree2.mostrar_nivel(binaryTree2.raiz);
+                    */
                 case 12:
-                   System.out.println("Comparar arboles");
+                   System.out.println("Arbol Completo"+escompleto(resultado.raiz));
+                          
                    break;
                 case 13:
                    salir=true;
